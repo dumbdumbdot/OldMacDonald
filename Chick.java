f396d0a06c1ba8544686961f11ec2fb8e72fddac
dumbdumbdot <christinewong324@gmail.com>
2017-10-18 14:22:44 -0700

    finished assignment

--- Chick.java
+++ Chick.java
@@ -1,9 +1,12 @@
 class Chick implements Animal {     
      private String myType;     
      private String mySound;      
-     public Chick(String type, String sound)     {         
-         myType = type;         
-         mySound = sound;     
+     public Chick(String type, String sound, String sound1)     {         
+         myType = type;  
+         if((int)(Math.random()*2) == 0)
+             mySound = sound; 
+         else
+             mySound = sound1;
      }     
      public Chick()     {         
          myType = "unknown";         
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,10 @@
 
 void setup()
 {
-	 Cow c = new Cow("cow", "moo");   
+	 Cow c = new Cow("cow", "moo");  
+   Chick a = new Chick("chick", "cluck", "cheep");
+   Pig b = new Pig("pig", "oink");
    System.out.println(c.getType() + " goes " + c.getSound()); 
+   Farm f = new Farm();
+   f.animalSounds(); 
 }
